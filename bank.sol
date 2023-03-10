ad88c148e12f095a1429ee5f5f67523a9ed97965
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2023-03-09 20:02:55 -0500

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -2,22 +2,26 @@
 pragma solidity ^0.8.0;
 
 contract bank{
-    mapping(address => uint256)balance;
+    mapping(address => uint256)balances;
     
 
     function deposit() external payable{
-        balance[msg.sender] += msg.value;
+        balances[msg.sender] += msg.value;
     }
 
     function getBalance() public view returns (uint256){
-        return balance[msg.sender];
+        return balances[msg.sender];
     }
 
     function withdraw() external{
-        uint256 amt = balance[msg.sender];
-        msg.sender.call{value: amt}("");
-        balance[msg.sender] = 0;
+        uint256 amt = balances[msg.sender];
+        (bool success, ) = msg.sender.call{value: amt}(""); // must check return value of a low-level call
+        if(!success) revert();
+        balances[msg.sender] = 0;
     }
 
-    
+receive() payable external 
+{
+
+}   
 }

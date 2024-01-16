4095ef4649426abf6d172bc99f391590829b649e
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-01-16 13:14:22 -0500

    Update TokenB.sol

--- src/reorg-attack/TokenB.sol
+++ src/reorg-attack/TokenB.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC4626} from "./ERC4626.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TokenB is ERC4626, Ownable(msg.sender) {

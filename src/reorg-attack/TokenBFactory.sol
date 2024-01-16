21fabea09e907219279c40ec22ef36ed7bfd5dad
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-01-16 13:11:06 -0500

    Create TokenBFactory.sol

---
+++ src/reorg-attack/TokenBFactory.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {TokenB} from "./TokenB.sol";
+
+
+
+
+contract TokenBFactory {
+ 
+    function deployTokenB(address assetToken, bytes32 _salt) public returns (TokenB tokenB){
+              tokenB = new TokenB{salt: _salt}(IERC20(assetToken));
+              tokenB.transferOwnership(msg.sender);
+    }
+}
+ 

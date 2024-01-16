4e10327e5a8c290c53536d7020926c59c37a4198
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-01-16 11:22:55 -0500

    Update PPSwap.sol

--- src/first-depositor-attack/PPSwap.sol
+++ src/first-depositor-attack/PPSwap.sol
@@ -1,1 +1,12 @@
 
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+contract PPSwap is ERC20{
+    constructor() ERC20("PPSwap", "PPS"){
+        _mint(msg.sender, 1_000_000_000 ether);
+    }    
+}

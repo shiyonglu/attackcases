194a37ecac536762c6cc12f5b99a9f19da14d789
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-01-31 21:50:07 -0500

    Update SelfDestruct.sol

--- src/self-destruct/SelfDestruct.sol
+++ src/self-destruct/SelfDestruct.sol
@@ -52,7 +52,7 @@ contract Attack {
         // the game balance >= 7 ether
 
         // cast address to payable
-        address payable addr = payable(address(etherGame));
+        // address payable addr = payable(address(etherGame));
         // selfdestruct(addr);
     }
 }

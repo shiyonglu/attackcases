28ff7e571466e14dcce5ba259ec43dd695fda43d
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-02-08 20:06:08 -0500

    Update TokenBFactory.sol

--- src/first-depositor-attack/TokenBFactory.sol
+++ src/first-depositor-attack/TokenBFactory.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.17;
 
 import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import {TokenB} from "./TokenB.sol";
+import {PPSVault} from "./PPSVault.sol";
 
-contract TokenBFactory {
+contract PPSVaultFactory {
     
 
-    function deployTokenB(address assetToken) public returns (TokenB tokenB){
-            return new TokenB(IERC20(assetToken));
+    function deployPPSVault(address assetToken) public returns (PPSVault ppsVault){
+            return new PPSVault(IERC20(assetToken));
     }
 }
  

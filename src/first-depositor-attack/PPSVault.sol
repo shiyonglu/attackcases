5cdab5aa4a387de9960c179d6731e160a3ab5bfe
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-02-08 20:04:53 -0500

    Update and rename TokenB.sol to PPSVault.sol

--- src/first-depositor-attack/PPSVault.sol
+++ src/first-depositor-attack/PPSVault.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from "./ERC4626.sol";
 
-contract TokenB is ERC4626{
+contract Token is ERC4626{
     constructor(IERC20 _asset) ERC20("TokenB", "BBB") ERC4626(_asset){
 
     }
